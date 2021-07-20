8a62a88448cd541f6d93df14ed8500aa2e47bff7
Guilherme Garcia <guillherme966@gmail.com>
2021-07-19 21:48:04 -0300

    :sparkles: feat: First commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { useState } from "react";
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+  return (
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+      <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
+    </>
+  );
+}
